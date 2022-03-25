c065398458625f53ac4bb24ea24c08bc3d67469f
AguirreEze <aguirre.eze95@gmail.com>
2022-03-25 11:18:43 -0300

    Feat styles on module

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import api from "./api";
 import Product from "./components/Product";
 import { ProductType } from "./types";
+import styles from "./styles.module.scss";
 
 function App() {
   const [products, setProducts] = useState<ProductType[]>([]);
@@ -12,17 +13,17 @@ function App() {
   }, []);
 
   return (
-    <main>
-      <header>Estampitiency</header>
-      <section>
+    <main className={styles.main}>
+      <header className={styles.main}>Estampitiency</header>
+      <section className={styles.stand}>
         {products.map((product) => (
           <Product key={product.id} product={product} />
         ))}
       </section>
-      <aside>
+      <aside className={styles.cart}>
         <button>3 productos (total: $12)</button>
       </aside>
-      <footer>
+      <footer className={styles.footer}>
         Encontrá la consigna de este ejercicio y otros más{" "}
         <a href="https://github.com/goncy/interview-challenges/tree/main/simple-cart">
           acá
